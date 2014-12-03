82dd1cc5fad1137147011df9b849348b72536939
alexander <ksiusha5@gmail.com>
2014-12-03 19:23:59 +0200

    start

---
+++ js/all.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('input[placeholder], textarea[placeholder]').placeholder();
+
+  // $('.video-types .v-type-title h3').mouseenter(function(){
+  // 	$('.video-types .v-type-wrap .img-shadow').css('background', '#fff');
+  // });
+});
